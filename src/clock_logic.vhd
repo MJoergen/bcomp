501104945b9bbe5eaf25f8ef66947c21cc5afd50
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2017-04-04 13:17:57 +0200

    New block.

---
+++ src/clock_logic.vhd
@@ -0,0 +1,64 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+-- clock_logic.vhd
+-- This entity implements the feature described in the video
+-- https://www.youtube.com/watch?v=SmQ5K7UQPMM
+
+entity clock_logic is
+
+    generic (
+                COUNTER_SIZE_MONO : integer := 16; -- for delay
+                COUNTER_SIZE_BI   : integer := 16  -- for delay
+            );
+
+    port (
+             -- Clock input from crystal (for delay)
+             clk_i       : in  std_logic;
+
+             -- Switch input
+             sw_i        : in  std_logic;
+
+             -- Push button input
+             btn_i       : in  std_logic;
+
+             -- Halt input
+             hlt_i       : in  std_logic;
+
+             -- The derived clock output
+             clk_deriv_o : out std_logic);
+
+end clock_logic;
+
+architecture Structural of clock_logic is
+    signal btn_delay : std_logic;
+    signal sw_reg    : std_logic;
+
+begin
+    -- Instantiate monostable
+    inst_monostable_clock : entity work.monostable_clock
+    generic map (
+                    COUNTER_SIZE => COUNTER_SIZE_MONO
+                )
+    port map (
+                 clk_i       => clk_i  ,
+                 btn_i       => btn_i  ,
+                 btn_delay_o => btn_delay
+             );
+
+    -- Instantiate bistable
+    inst_bistable_clock : entity work.bistable_clock
+    generic map (
+                    COUNTER_SIZE => COUNTER_SIZE_BI
+                )
+    port map (
+                 clk_i    => clk_i  ,
+                 sw_i     => sw_i   ,
+                 sw_reg_o => sw_reg
+             );
+
+    clk_deriv_o <= ((clk_i and sw_reg) or (btn_delay and not sw_reg)) and not hlt_i;
+
+end Structural;
+
---
+++ src/clock_logic_tb.vhd
@@ -0,0 +1,80 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity clock_logic_tb is
+end clock_logic_tb ;
+
+architecture Structural of clock_logic_tb is
+
+    -- Clock
+    signal clk : std_logic; -- 25 MHz
+    signal test_running : boolean := true;
+
+    -- Inputs to the DUT
+    signal sw        : std_logic;
+    signal btn       : std_logic;
+    signal hlt       : std_logic;
+
+    -- Output from the DUT
+    signal clk_deriv : std_logic;
+
+    signal counter   : std_logic_vector(7 downto 0);
+
+begin
+    -- Generate clock
+    clk_gen : process
+    begin
+        if not test_running then
+            wait;
+        end if;
+
+        clk <= '1', '0' after 20 ns;
+        wait for 40 ns;
+    end process clk_gen;
+
+    -- Instantiate DUT
+    inst_clock_logic : entity work.clock_logic
+    generic map (
+                COUNTER_SIZE_MONO => 4,
+                COUNTER_SIZE_BI   => 4
+                    -- 40 ns * 2^4 = 640 ns.
+                )
+    port map (
+                 clk_i       => clk       ,
+                 sw_i        => sw        ,
+                 btn_i       => btn       ,
+                 hlt_i       => hlt       ,
+                 clk_deriv_o => clk_deriv 
+             );
+
+    process(clk_deriv)
+    begin
+        if hlt = '1' then
+            counter <= (others => '0');
+        elsif rising_edge(clk_deriv) then
+            counter <= counter + 1;
+        end if;
+    end process;
+
+
+    -- Start the main test
+    main_test : process is
+    begin
+        hlt <= '1';
+        sw  <= '1';
+        wait for 1500 ns;
+
+        hlt <= '0';
+        sw  <= '1';
+        wait for 1500 ns;
+
+        sw  <= '0';
+        wait for 1500 ns;
+
+        test_running <= false;
+        wait;
+    end process main_test;
+
+end Structural;
+
