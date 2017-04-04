bcd2a02c26546766adf5b435d8b6331bcc288234
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2017-04-04 17:32:29 +0200

    Added new block.

---
+++ src/register_8bit.vhd
@@ -0,0 +1,49 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+-- bistable_clock.vhd
+-- This entity implements the feature described in the video
+-- https://www.youtube.com/watch?v=WCwJNnx36Rk
+-- The implementation is quite different from the video because of the
+-- way the switches are hooked up on the Basys2 board. In the video,
+-- he uses a double throw switch, but the Basys2 board only has 
+-- single throw switches. So instead we implement debouncing
+-- uses a regular timer, based on the crystal clock.
+
+entity register_8bit is
+
+    port (
+             -- Clock input from crystal (for delay)
+             clk_i       : in  std_logic;
+
+             -- Data input
+             data_i      : in  std_logic_vector(7 downto 0);
+
+             -- Data output
+             data_o      : out std_logic_vector(7 downto 0);
+
+             -- Control inputs
+             load_i      : in  std_logic;
+             enable_i    : in  std_logic);
+
+end register_8bit;
+
+architecture Structural of register_8bit is
+
+    signal data : std_logic_vector(7 downto 0);
+
+begin
+    process(clk_i)
+    begin
+        if rising_edge(clk_i) then
+            if load_i = '1' then
+                data <= data_i;
+            end if;
+        end if;
+    end process;
+
+    data_o <= data when (enable_i = '1') else "ZZZZZZZZ";
+
+end Structural;
+
---
+++ src/register_8bit_tb.vhd
@@ -0,0 +1,100 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity register_8bit_tb is
+end register_8bit_tb ;
+
+architecture Structural of register_8bit_tb is
+
+    -- Clock
+    signal clk : std_logic; -- 25 MHz
+    signal test_running : boolean := true;
+
+    -- Inputs to the DUT
+    signal data_in    : std_logic_vector(7 downto 0);
+    signal load       : std_logic;
+    signal enable     : std_logic;
+
+    -- Output from the DUT
+    signal data_out   : std_logic_vector(7 downto 0);
+
+begin
+    -- Generate clock
+    clk_gen : process
+    begin
+        if not test_running then
+            wait;
+        end if;
+
+        clk <= '1', '0' after 20 ns;
+        wait for 40 ns;
+    end process clk_gen;
+
+    -- Instantiate DUT
+    inst_register_8bit : entity work.register_8bit
+    port map (
+                 clk_i       => clk       ,
+                 data_i      => data_in   ,
+                 data_o      => data_out  ,
+                 load_i      => load      ,
+                 enable_i    => enable
+             );
+
+    -- Start the main test
+    main_test : process is
+    begin
+        load    <= '1'; -- Start with a known value
+        enable  <= '1';
+        data_in <= "01010101";
+
+        wait for 80 ns;
+        assert data_out = "01010101";
+
+        data_in <= "00001111";
+        wait for 80 ns;
+        assert data_out = "00001111";
+
+        data_in <= "00110011";
+        wait for 80 ns;
+        assert data_out = "00110011";
+
+        load    <= '0';
+        wait for 80 ns;
+        assert data_out = "00110011";
+
+        data_in <= "10101010";
+        wait for 80 ns;
+        assert data_out = "00110011";
+
+        enable  <= '0';
+        wait for 80 ns;
+        assert data_out = "ZZZZZZZZ";
+
+        enable  <= '1';
+        wait for 80 ns;
+        assert data_out = "00110011";
+
+        load    <= '1'; -- Start with a known value
+        wait for 80 ns;
+        assert data_out = "10101010";
+
+        enable  <= '0';
+        wait for 80 ns;
+        assert data_out = "ZZZZZZZZ";
+
+        data_in <= "11001100";
+        load    <= '1'; -- Start with a known value
+        wait for 80 ns;
+        assert data_out = "ZZZZZZZZ";
+
+        enable  <= '1';
+        wait for 80 ns;
+        assert data_out = "11001100";
+
+        test_running <= false;
+        wait;
+    end process main_test;
+
+end Structural;
+
