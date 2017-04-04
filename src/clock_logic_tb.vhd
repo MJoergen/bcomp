9581afb93b26a4ff8e37829d41efc65267b5a752
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2017-04-04 16:22:53 +0200

    Improved test cases.

--- src/clock_logic_tb.vhd
+++ src/clock_logic_tb.vhd
@@ -12,14 +12,15 @@ architecture Structural of clock_logic_tb is
     signal test_running : boolean := true;
 
     -- Inputs to the DUT
-    signal sw        : std_logic;
-    signal btn       : std_logic;
-    signal hlt       : std_logic;
+    signal sw         : std_logic;
+    signal btn        : std_logic;
+    signal hlt        : std_logic;
 
     -- Output from the DUT
-    signal clk_deriv : std_logic;
+    signal clk_deriv  : std_logic;
 
-    signal counter   : std_logic_vector(7 downto 0);
+    signal counter    : std_logic_vector(7 downto 0);
+    signal cnt_test   : std_logic_vector(7 downto 0);
 
 begin
     -- Generate clock
@@ -62,15 +63,42 @@ begin
     main_test : process is
     begin
         hlt <= '1';
-        sw  <= '1';
+        sw  <= '1'; -- Use crystal clock
+        btn <= '0';
+        wait for 1500 ns; -- Wait until halt debounces.
+
+        cnt_test <= counter;
         wait for 1500 ns;
+        assert cnt_test = counter;
 
         hlt <= '0';
-        sw  <= '1';
-        wait for 1500 ns;
+        sw  <= '1'; -- Use crystal clock
+        wait for 1500 ns; -- Wait until switch debounces.
 
-        sw  <= '0';
+        cnt_test <= counter; -- Verify clock is running.
+        wait for 1500 ns; 
+        assert counter > cnt_test + 1;
+
+        sw  <= '0'; -- Use manual clock
+        wait for 1500 ns; -- Wait until switch debounces.
+
+        cnt_test <= counter; -- Verify clock is stopped.
         wait for 1500 ns;
+        assert cnt_test = counter;
+
+        cnt_test <= counter; -- Verify clock is ticked only once.
+        btn <= '1';
+        wait for 1500 ns; -- Wait until button debounces.
+        assert counter = cnt_test + 1;
+
+        cnt_test <= counter; -- Verify clock is stopped.
+        wait for 1500 ns;
+        assert cnt_test = counter;
+
+        cnt_test <= counter; -- Verify clock is stopped.
+        btn <= '0';
+        wait for 1500 ns;
+        assert cnt_test = counter;
 
         test_running <= false;
         wait;
