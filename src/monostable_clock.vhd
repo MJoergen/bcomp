eb19e0651688338f78334f286ba07c52fd71a38e
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2017-04-04 12:11:04 +0200

    Renamed again.

--- src/monostable_clock.vhd
+++ src/monostable_clock.vhd
@@ -2,60 +2,59 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
--- astable_clock.vhd
+-- monostable_clock.vhd
 -- This entity implements the feature described in
--- the video https://www.youtube.com/watch?v=kRlSFm519Bo
+-- the video https://www.youtube.com/watch?v=81BgFhm2vz8
 -- The implementation is quite different from the video
 -- because of the limitations of the FPGA, so we
 -- need access to the crystal clock
 -- Once every millisecond the push button is stored in a register,
 -- and this provides the debouncing functionality.
 
-entity monostable_clock is
+entity bistable_clock is
 
     generic (
-                COUNTER_SIZE : integer := 16 -- for debouncing.
-                -- 25 MHZ / 2^16 = 381 Hz, corresponding
-                -- to a period of 2.6 ms.
+                COUNTER_SIZE : integer := 21 -- for delay
+                -- 40 ns * 2^21 = 0.1 s
             );
 
     port (
-             -- Clock input from crystal (for debouncing)
-             clk_i     : in  std_logic;
+             -- Clock input from crystal (for delay)
+             clk_i       : in  std_logic;
 
              -- Button input
-             btn_i     : in  std_logic;
+             btn_i       : in  std_logic;
 
              -- Registered button output
-             btn_reg_o : out std_logic);
+             btn_delay_o : out std_logic);
 
-end monostable_clock;
+end bistable_clock;
 
-architecture Structural of monostable_clock is
-    signal counter : std_logic_vector(COUNTER_SIZE-1 downto 0) :=
-                     (others => '0');
-    signal btn_reg : std_logic;
-    constant ZERO  : std_logic_vector(COUNTER_SIZE-1 downto 0) :=
-                     (others => '0');
+architecture Structural of bistable_clock is
+    signal counter   : std_logic_vector(COUNTER_SIZE-1 downto 0) :=
+                       (others => '0');
+    signal btn_delay : std_logic;
+    constant ZERO    : std_logic_vector(COUNTER_SIZE-1 downto 0) :=
+                       (others => '0');
+    constant ONES    : std_logic_vector(COUNTER_SIZE-1 downto 0) :=
+                       (others => '1');
 
 begin
-    process(clk_i) -- Just a simple counter.
+    process(clk_i)
     begin
         if rising_edge(clk_i) then
-            counter <= counter + 1;
-        end if;
-    end process;
-
-    process(clk_i) -- Sample the button input at regular intervals.
-    begin
-        if rising_edge(clk_i) then
-            if counter = ZERO then
-                btn_reg <= btn_i;
+            if btn_i = '1' then
+                btn_delay <= '1';
+                counter <= ZERO;
+            elsif counter = ONES then
+                btn_delay <= '0';
+            else
+                counter <= counter + 1;
             end if;
         end if;
     end process;
 
-    btn_reg_o <= btn_reg;
+    btn_delay_o <= btn_delay;
 
 end Structural;
 
--- src/monostable_clock_tb.vhd
+++ src/monostable_clock_tb.vhd
@@ -2,18 +2,18 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-entity monostable_clock_tb is
-end monostable_clock_tb ;
+entity bistable_clock_tb is
+end bistable_clock_tb ;
 
-architecture Structural of monostable_clock_tb is
+architecture Structural of bistable_clock_tb is
 
     -- Clock
     signal clk : std_logic; -- 25 MHz
     signal test_running : boolean := true;
 
     -- Push button
-    signal btn     : std_logic;
-    signal btn_reg : std_logic;
+    signal btn       : std_logic;
+    signal btn_delay : std_logic;
 
 
 begin
@@ -29,15 +29,15 @@ begin
     end process clk_gen;
 
     -- Instantiate DUT
-    inst_monostable_clock : entity work.monostable_clock
+    inst_bistable_clock : entity work.bistable_clock
     generic map (
                     COUNTER_SIZE => 4 -- Much shorter counter during simulation.
                     -- 40 ns * 2^4 = 640 ns.
                 )
     port map (
-                 clk_i     => clk    ,
-                 btn_i     => btn    ,
-                 btn_reg_o => btn_reg 
+                 clk_i       => clk    ,
+                 btn_i       => btn    ,
+                 btn_delay_o => btn_delay
              );
 
     -- Start the main test
@@ -45,37 +45,43 @@ begin
     begin
         wait for 80 ns;
 
-        btn <= '0';
+        btn <= '1';
+        wait for 40 ns;
+        assert btn_delay = '1';
+
         wait for 640 ns;
-        assert btn_reg = '0';
+        assert btn_delay = '1';
+
+        btn <= '0';
+        wait for 160 ns;
+        assert btn_delay = '1';
+        wait for 160 ns;
+        assert btn_delay = '1';
+        wait for 160 ns;
+        assert btn_delay = '1';
+        wait for 160 ns;
+        assert btn_delay = '0';
+
+        wait for 640 ns;
+        assert btn_delay = '0';
 
         btn <= '1';
-        wait for 640 ns;
-        assert btn_reg = '1';
+        wait for 40 ns;
+        assert btn_delay = '1';
 
         btn <= '0';
-        wait for 640 ns;
-        assert btn_reg = '0';
+        wait for 560 ns;
+        assert btn_delay = '1';
 
         btn <= '1';
-        wait for 160 ns;
-        assert btn_reg = '0';
-        wait for 160 ns;
-        assert btn_reg = '0';
-        wait for 160 ns;
-        assert btn_reg = '0';
-        wait for 160 ns;
-        assert btn_reg = '1';
+        wait for 40 ns;
+        assert btn_delay = '1';
 
         btn <= '0';
-        wait for 160 ns;
-        assert btn_reg = '1';
-        wait for 160 ns;
-        assert btn_reg = '1';
-        wait for 160 ns;
-        assert btn_reg = '1';
-        wait for 160 ns;
-        assert btn_reg = '0';
+        wait for 560 ns;
+        assert btn_delay = '1';
+        wait for 80 ns;
+        assert btn_delay = '0';
 
         test_running <= false;
         wait;
