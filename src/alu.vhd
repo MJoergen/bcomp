72f7443c38243c0f74a5206f77f5f4632726a204
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2017-04-05 19:56:42 +0200

    New block

---
+++ src/alu.vhd
@@ -0,0 +1,51 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+-- clock_logic.vhd
+-- This entity implements the feature described in the video
+-- https://www.youtube.com/watch?v=SmQ5K7UQPMM
+
+entity alu is
+
+    port (
+             -- Data inputs
+             areg_i      : in  std_logic_vector (7 downto 0);
+             breg_i      : in  std_logic_vector (7 downto 0);
+
+             -- Control input:
+             -- '0' for addition, '1' for subtraction.
+             sub_i       : in  std_logic;
+
+             -- Output enable
+             enable_i    : in  std_logic;
+
+             -- Result output
+             result_o    : out std_logic_vector (7 downto 0)
+         );
+
+end alu;
+
+architecture Structural of alu is
+
+    signal plus   : std_logic_vector (7 downto 0);
+    signal minus  : std_logic_vector (7 downto 0);
+    signal result : std_logic_vector (7 downto 0);
+
+begin
+
+    -- Intermediate calculations
+    -- Note. This is slightly cheating compared to the video
+    -- because we're here relying on the synthesis tool
+    -- using the chip's builtin adders and subtractors.
+    plus  <= areg_i + breg_i;
+    minus <= areg_i - breg_i;
+
+    -- Multiplex the correct result depending on the operation.
+    result <= minus when sub_i = '1' else plus;
+
+    -- The output is a tristate buffer.
+    result_o <= result when enable_i = '1' else (others => 'Z');
+
+end Structural;
+
---
+++ src/alu_tb.vhd
@@ -0,0 +1,71 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity alu_tb is
+end alu_tb ;
+
+architecture Structural of alu_tb is
+
+    -- Inputs to the DUT
+    signal areg       : std_logic_vector (7 downto 0);
+    signal breg       : std_logic_vector (7 downto 0);
+    signal sub        : std_logic;
+    signal enable     : std_logic;
+
+    -- Output from the DUT
+    signal result     : std_logic_vector (7 downto 0);
+
+begin
+    -- Instantiate DUT
+    inst_alu : entity work.alu
+    port map (
+                 areg_i      => areg      ,
+                 breg_i      => breg      ,
+                 sub_i       => sub       ,
+                 enable_i    => enable    ,
+                 result_o    => result
+             );
+
+    -- Start the main test
+    main_test : process is
+    begin
+        -- Clear all inputs
+        enable <= '0';
+        areg <= "00000000";
+        breg <= "00000000";
+        sub <= '0';
+        wait for 40 ns;
+        assert result = "ZZZZZZZZ";
+
+        areg <= "01010101";
+        breg <= "00110011";
+        sub <= '0';
+        wait for 40 ns;
+        assert result = "ZZZZZZZZ";
+
+        enable <= '1';
+        wait for 40 ns;
+        assert result = "10001000"; -- 0x55 + 0x33 = 0x88
+
+        enable <= '0';
+        wait for 40 ns;
+        assert result = "ZZZZZZZZ";
+
+        sub <= '1';
+        wait for 40 ns;
+        assert result = "ZZZZZZZZ";
+
+        enable <= '1';
+        wait for 40 ns;
+        assert result = "00100010"; -- 0x55 - 0x33 = 0x22
+
+        enable <= '0';
+        wait for 40 ns;
+        assert result = "ZZZZZZZZ";
+
+        wait;
+    end process main_test;
+
+end Structural;
+
